56e150181e362f913cc9f6225b2198bd33507443
Shuyou <shuyoou@outlook.com>
2025-11-12 17:33:50 +0800

    feat: add ads.txt and meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 
+	<meta name="google-adsense-account" content="ca-pub-7019817095267700" />
 	<script
 		async
 		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7019817095267700"
