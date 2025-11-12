d3ebd746e6891577372f9391c1c4cb41a5cbe429
Shuyou <shuyoou@outlook.com>
2025-11-12 17:15:23 +0800

    feat: add adSense script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7019817095267700"
+		crossorigin="anonymous"
+	></script>
 </svelte:head>
 
 {@render children?.()}
